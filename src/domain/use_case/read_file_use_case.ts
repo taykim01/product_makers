5e92135d8a274cf79dc5724b496395bce836faaa
Junhyoung <junh3951@me.com>
2024-03-30 11:51:58 +0900

    Feat: ReadFileUseCase 생성

---
+++ src/domain/use_case/read_file_use_case.ts
@@ -0,0 +1,21 @@
+import CodeResponse from "@/app/code_response"
+import { Result } from "@/app/types"
+
+export default class ReadFileUseCase {
+        readFile (file: any) :CodeResponse {
+        // pdƒ와 이미지를 raw text로 변환
+        // input : pdf 또는 img 파일
+        // output : raw text
+        const convert_file_to_text_service = new ConvertFileToTextService(file)
+        const response = convert_file_to_text_service
+        return new CodeResponse(response.result, response.errorcode, response.payload)
+    }
+    readExampleQuestion (file: any): CodeResponse {
+        // pdƒ와 이미지를 raw text로 변환
+        // input : pdf 또는 img 파일
+        // output : raw text
+        const convert_file_to_text_service = new ConvertFileToTextService(file)
+        const response = convert_file_to_text_service
+        return new CodeResponse(response.result, response.errorcode, response.payload)
+    }
+}
