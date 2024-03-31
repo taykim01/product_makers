a055acbd2921f87fa23e387bd0da897d0aa7c066
Junhyoung <junh3951@me.com>
2024-04-01 03:49:02 +0900

    Feat: ConvertFileToTextService 생성
    tesseract.js 패키지 추가

--- src/data/service/convert_file_to_text_service.ts
+++ src/data/service/convert_file_to_text_service.ts
@@ -1,5 +1,14 @@
-// 이준형
+import CodeResponse from '@/app/code_response';
+import { Result } from '@/app/types';
+import Tesseract from 'tesseract.js';
 
 export default class ConvertFileToTextService {
-  convert() {}
+  async convert(file: File): Promise<CodeResponse> {
+    try {
+      const { data } = await Tesseract.recognize(file, 'kor+eng');
+      return new CodeResponse(Result.SUCCESS, "변환성공", data.text);
+    } catch (error) {
+      return new CodeResponse(Result.ERROR, "변환 실패", error);
+    }
+  }
 }
