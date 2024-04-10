ea2960f9b03ec291d8c95255a4db308c4af999e7
Junhyoung <junh3951@me.com>
2024-04-11 01:01:48 +0900

    Feat: Firebase analytics 추가

--- src/data/firebase/index.tsx
+++ src/data/firebase/index.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -7,10 +8,12 @@ const firebaseConfig = {
     projectId: "kairos-3326d",
     storageBucket: "kairos-3326d.appspot.com",
     messagingSenderId: "85411976801",
-    appId: "1:85411976801:web:556d7bf83b27b43e02bc20"
+    appId: "1:85411976801:web:556d7bf83b27b43e02bc20",
+    measurementId: "G-GMLNZ1C0ZS"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-export { db };
+export { db, analytics };
